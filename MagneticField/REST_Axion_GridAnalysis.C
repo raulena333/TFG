cb6feba6785c6c71a911beaaf6fbc57e383b26d1
raulena333 <raulena333@gmail.com>
2024-04-02 14:10:15 +0200

    Minor Improvement

--- MagneticField/REST_Axion_GridAnalysis.C
+++ MagneticField/REST_Axion_GridAnalysis.C
@@ -16,7 +16,7 @@
 
 //*******************************************************************************************************
 //*** Description: This script performs analysis on different magnetic field maps represented as grids with varying 
-//*** mesh sizes. It also calculates the transmission probability, error and runtime for each grid configuration at 
+//*** mesh sizes. It also calculates the transmission probability, error, and runtime for each grid configuration at 
 //*** different axion masses 
 //*** 
 //*** Mesh Map Definitions in mm:
@@ -37,8 +37,8 @@
 //*******************************************************************************************************
 
 struct FieldTrack {
-    std::unique_ptr<TRestAxionMagneticField> magneticField; // Using unique_ptr
-    std::unique_ptr<TRestAxionField> axionField; // Using unique_ptr
+    std::unique_ptr<TRestAxionMagneticField> magneticField;
+    std::unique_ptr<TRestAxionField> axionField;
     const TVector3 mapSize;
     std::vector<double> probability;
     std::vector<double> error;
@@ -50,20 +50,19 @@ struct FieldTrack {
 
 constexpr bool kDebug = true;
 
-Int_t REST_Axion_GridAnalysis(Int_t nData = 3, Double_t Ea = 4.2, std::string gasName = "He", Double_t m1 = 0.01, Double_t m2 = 0.1) {
+Int_t REST_Axion_GridAnalysis(Int_t nData = 100, Double_t Ea = 4.2, std::string gasName = "He", Double_t m1 = 0.01, Double_t m2 = 0.1) {
     // Mesh Map Definitions in mm
     std::vector<TVector3> meshSizes = {
-        TVector3(10,10,50),
         TVector3(20, 20, 100),
         TVector3(30, 30, 150),
         TVector3(50, 50, 250),
-        TVector3(100, 100, 500)
+        TVector3(50, 50, 500)
     };
 
     // Create Variables
     std::vector<std::string> fieldNames = {"babyIAXO_2024_cutoff", "babyIAXO_2024"};
     Double_t gasDensity = 2.9836e-10;
-    TVector3 position(-100, -100 ,-11000);
+    TVector3 position(-100, -100, -11000);
     TVector3 direction = (position - TVector3(10, -10 , 9000)).Unit();
 
     // Create an instance of TRestAxionBufferGas if gasName is provided
@@ -83,34 +82,41 @@ Int_t REST_Axion_GridAnalysis(Int_t nData = 3, Double_t Ea = 4.2, std::string ga
     }
 
     for(const auto& fieldName : fieldNames) {
-        for (const auto& ma : masses) {
-            // Fill the struct 
-            std::map<std::string, FieldTrack> fields;
-            for (size_t i = 0; i < meshSizes.size(); ++i) {
-                std::string gridName = "Grid" + std::to_string(i + 1);
-                fields.emplace(gridName, FieldTrack{
-                    std::make_unique<TRestAxionMagneticField>("fields.rml", fieldName), 
-                    std::make_unique<TRestAxionField>(),
-                    meshSizes[i]
-                });
-            }
+        // Fill the struct 
+        std::map<std::string, FieldTrack> fields;
+        for (size_t i = 0; i < meshSizes.size(); ++i) {
+            std::string gridName = "Grid" + std::to_string(i + 1);
+            fields.emplace(gridName, FieldTrack{
+                std::make_unique<TRestAxionMagneticField>("fields.rml", fieldName), 
+                std::make_unique<TRestAxionField>(),
+                meshSizes[i]
+            });
+        }
 
-            // Create an instance of TRestAxionField and assign magnetic field and gas (if provided). Remap the grid
-            for (auto& field : fields) {
-                if (gas != nullptr) 
-                    field.second.axionField->AssignBufferGas(gas.get());
-                for (size_t n = 0; n < field.second.magneticField->GetNumberOfVolumes(); n++) {
-                    field.second.magneticField->ReMap(n, field.second.mapSize);
-                }
-                field.second.magneticField->SetTrack(position, direction);
-                field.second.axionField->AssignMagneticField(field.second.magneticField.get());
-            }  
+        // Create an instance of TRestAxionField and assign magnetic field and gas (if provided). Remap the grid
+        for (auto& field : fields) {
+            if (gas != nullptr) 
+                field.second.axionField->AssignBufferGas(gas.get());
+            for (size_t n = 0; n < field.second.magneticField->GetNumberOfVolumes(); n++) {
+                field.second.magneticField->ReMap(n, field.second.mapSize);
+            }
+            field.second.magneticField->SetTrack(position, direction);
+            field.second.axionField->AssignMagneticField(field.second.magneticField.get());
+        }  
+
+        for (const auto& ma : masses) {
+            if(kDebug){
+                std::cout << "+--------------------------------------------------------------------------+" << std::endl;
+                std::cout << "Mass: " << ma << std::endl;
+                std::cout << "+--------------------------------------------------------------------------+" << std::endl;
+                std::cout << std::endl;
+            }
 
             // Iterate over each grid map mesh
             for(unsigned i = 0; i < nData; i++) {
                 if(kDebug){
                     std::cout << "+--------------------------------------------------------------------------+" << std::endl;
-                    std::cout << "Data: " << i << "/t Mass: " << ma << std::endl;
+                    std::cout << "Data: " << i << std::endl;
                     std::cout << "+--------------------------------------------------------------------------+" << std::endl;
                     std::cout << std::endl;
                 }
